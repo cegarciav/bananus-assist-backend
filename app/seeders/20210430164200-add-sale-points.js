cf1ae1cc8dd7c198a6419824b295e868a42ac95c
Matias <micea@uc.cl>
2021-04-30 13:03:31 -0400

    Seed complete

---
+++ app/seeders/20210430164200-add-sale-points.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     const sale_pointsArray = [];
+
+     sale_pointsArray.push({
+      id_store: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    return queryInterface.bulkInsert('sale_points', sale_pointsArray);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
