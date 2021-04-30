2024c324b413f1c5d335ed86c61054769b9f0ce6
Matias <micea@uc.cl>
2021-04-30 01:46:52 -0400

    Create complete, read complete, delete complete

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,54 +1,15 @@
 "use strict"
-const express = require('express')
-
+const express = require('express');
 const router = express.Router();
-
-const Models = require('../models');
-const user = Models.user;
-
-const PERMITED_FIELD = [
-    'name',
-    'password',
-    'email'
-];
+const handlers = require("../controllers/user_controller");
 
 //CREATE
-router.route('/').post( async(req, res) => {
-    console.log(req.body);
-    try{
-        await user.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-        });
-        res.json({
-            state: 'OK'
-        });
-    } catch(error){
-        res.json({
-            state: 'F',
-            error: error,
-        });
-    }
-});
-
+router.post("/", handlers.create);
 //READ ALL
-router.route('/').get( async(req, res) => {
-    const users = await user.findAll();
-    res.json(users);
-});
-
+router.get("/", handlers.show_all);
 //READ ONE
-router.route('/:id').get( async(req, res) => {
-    const user_id = await user.findOne({ 
-        where: {
-            id: req.params.id,
-        }
-    });
-    res.json(user_id);
-});
-
-
-
+router.get("/:id", handlers.show_one);
+//DELETE
+router.delete("/:id", handlers.delete);
 
 module.exports = router;
