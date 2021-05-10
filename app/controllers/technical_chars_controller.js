0484ad6e1ad0420f1964499c1d9e98d83d43042e
cegarciav <cegarcia2@uc.cl>
2021-05-10 19:35:30 -0400

    improvement: try-catch covering whole function

--- app/controllers/technical_chars_controller.js
+++ app/controllers/technical_chars_controller.js
@@ -2,53 +2,82 @@ const technical_char = require('../models').technical_char;
 
 //CREATE
 async function create(req, res) {
-    if(!req.body.key || !req.body.value){
-        return res.status(400).json({state:"F", error: "Invalid fields"});
-    } 
-    try{
+    try {
+        if(!req.body.key || !req.body.value || !req.body.productId){
+            res.status(400).json({state:"F", error: "Invalid fields"});
+            return;
+        }
         await technical_char.create({
             key: req.body.key,
-            value: req.body.value
+            value: req.body.value,
+            productId: req.body.productId
         });
         res.status(201).json({
             state: 'OK'
         });
-    } catch(error){
+        return;
+    }
+    catch (error) {
         res.status(500).json({
             state: 'F',
             error: error,
         });
     }
-};
+}
 
 //READ ALL
 async function show_all(req, res) {
-    const technical_chars = await technical_char.findAll();
-    res.status(200).json(technical_chars);
-};
+    try {
+        const technical_chars = await technical_char.findAll();
+        res.status(200).json(technical_chars);
+        return;
+    }
+    catch (error) {
+        res.status(500).json({
+            state: 'F',
+            error: error,
+        });
+        return;
+    }
+}
 
 //READ ONE
-async function  show(req, res) {
-    if (!req.body.id){
-        return res.status(400),json({state:"F", error:"Invalid fields"})
+async function show(req, res) {
+    try {
+        if (!req.body.id){
+            res.status(400).json({state:"F", error:"Invalid fields"});
+            return;
+        }
+        const technical_char_id = await technical_char.findOne({
+            where: {id: req.body.id}});
+        if(!technical_char_id){
+            res.status(400).json({state:"F", error:"Technical characteristic doesn't exist"});
+            return;
+        }
+        res.status(200).json(technical_char_id);
+        return;
     }
-    const technical_char_id = await technical_char.findOne({ 
-        where: {id: req.params.id}});
-    if(!technical_char_id){
-        return res.status(400).json({state:"F", error:"Tech char doesn't exist"})
+    catch (error) {
+        res.status(500).json({
+            state: 'F',
+            error: error,
+        });
+        return;
     }
-    res.status(200).json(technical_char_id);
-};
+}
+
 //UPDATE
 async function update(req, res){
-    if (!req.body.id){
-        return res.status(400),json({state:"F", error:"Invalid fields"})
-    }
-    let current_char = await technical_char.findOne({where : {id:req.body.id}})
-    if (!current_char){
-        return res.status(400).json({state: 'F',error: 'Technical characteristic doesnt exist'});
-    }
-    try{
+    try {
+        if (!req.body.id){
+            res.status(400).json({state:"F", error:"Invalid fields"});
+            return;
+        }
+        let current_char = await technical_char.findOne({where : {id:req.body.id}});
+        if (!current_char){
+            res.status(400).json({state: 'F',error: 'Technical characteristic doesn\'t exist'});
+            return;
+        }
         await technical_char.update({
             key: ((req.body.key)? req.body.key: current_char.key),
             value: ((req.body.value)? req.body.vale:current_char.value)
@@ -56,42 +85,47 @@ async function update(req, res){
         });
 
         res.status(200).json({state: 'OK'});
+        return;
+    }
+    catch (error) {
+        res.status(500).json({
+            state: 'F',
+            error: error,
+        });
+        return;
+    }
+}
 
-    } catch (error){
-        
+//DELETE
+async function cdelete(req, res) {
+    try {
+        if (!req.body.id){
+            res.status(400).json({state:"F", error:"Invalid fields"});
+            return;
+        }
+        let current_char = await technical_char.findOne({where : {id:req.body.id}});
+        if (!current_char){
+            res.status(400).json({state: 'F',error: 'Technical characteristic doesn\'t exist'});
+            return;
+        }
+
+        const udestroy = await technical_char.destroy({ 
+            where: {
+                id: req.body.id,
+            }
+        });
+        res.status(200).json({
+            state: 'OK'
+        });
+        return;
+    }
+    catch (error) {
         res.status(500).json({
             state: 'F',
             error: error,
         });
     }
 }
-//DELETE
-async function  cdelete(req, res) {
-    if (!req.body.id){
-        return res.status(400),json({state:"F", error:"Invalid fields"})
-    }
-    let current_char = await technical_char.findOne({where : {id:req.body.id}})
-    if (!current_char){
-        return res.status(400).json({state: 'F',error: 'Technical characteristic doesnt exist'});
-    }
-
-    try{
-        const udestroy = await technical_char.destroy({ 
-            where: {
-                id: req.params.id,
-            }
-        });
-        res.json({
-            state: 'OK'
-        });
-    } catch (error){
-        res.json({
-            state: 'F',
-            error: error,
-        });
-    }
-    
-};
 
 module.exports = {
     show_all: show_all,
@@ -99,5 +133,4 @@ module.exports = {
     create: create,
     update: update,
     delete: cdelete,
-  };
-  
+};
