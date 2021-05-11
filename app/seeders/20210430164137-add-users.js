a7d62e8074fcda62f4ce440df31323ef9def7c47
mafillol <mafillol@uc.cl>
2021-05-10 22:36:15 -0400

    feature: add users seeds

--- app/seeders/20210430164137-add-users.js
+++ app/seeders/20210430164137-add-users.js
@@ -1,4 +1,9 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const PASSWORD_SALT = parseInt(process.env.PASSWORD_SALT);
+const faker = require('faker');
+faker.locale = "es";
+faker.seed(123);
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -11,16 +16,20 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-     const usersArray = [];
 
-     usersArray.push({
-      name: 'Matias',
-      email: 'micea@uc.cl',
-      password: '123',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    return queryInterface.bulkInsert('users', usersArray);
+     const usersData= [];
+
+    for (let i = 0; i < 15; i += 1) {
+      usersData.push({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: bcrypt.hashSync(faker.internet.password(), PASSWORD_SALT),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+   return queryInterface.bulkInsert('users', usersData);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -30,5 +39,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     await queryInterface.bulkDelete('users', null, {})
   }
 };
