dcec53fbe8a73c8746dab6264fae4f0270ebd496
juakoke <jamunoz30@uc.cl>
2021-05-10 20:20:28 -0400

    feature:login, logout and some restriccions

---
+++ app/routes/session.js
@@ -0,0 +1,10 @@
+"use strict"
+const express = require('express');
+const router = express.Router();
+const handlers = require("../controllers/session_controller");
+const filters = handlers.filters
+
+router.post("/", filters.only_unlogged, handlers.log_in);
+router.delete("/", filters.only_logged, handlers.log_out)
+
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -8,6 +8,7 @@ const sale_points = require('./routes/sale_points');
 const stores = require('./routes/stores');
 const products = require('./routes/products');
 const technical_chars = require('./routes/technical_chars');
+const sessions = require('./routes/session')
 
 const db = require( './models' );
 const app = express();
@@ -20,14 +21,16 @@ app.use(cors({
   origin: frontendClient,
   optionsSuccessStatus: 200
 }));
+
 app.use(express.json()); 
+app.use(require("./controllers/session_controller").check_session)
 app.use('/', index);
 app.use('/users', users);
 app.use('/sale-points', sale_points);
 app.use('/stores', stores);
 app.use('/products', products);
 app.use('/chars', technical_chars);
-
+app.use('/sessions', sessions)
 
 db.sequelize
   .authenticate()
