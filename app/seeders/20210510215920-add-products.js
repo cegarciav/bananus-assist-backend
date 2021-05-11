af93a7e0c9622d68fca3442d91a9b4f4679015c8
mafillol <mafillol@uc.cl>
2021-05-10 22:36:48 -0400

    fearure: add products seeds

---
+++ app/seeders/20210510215920-add-products.js
@@ -0,0 +1,42 @@
+'use strict';
+const faker = require('faker');
+faker.locale = "es";
+faker.seed(123);
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     const productsData = [];
+
+    for (let i = 0; i < 40; i += 1) {
+      productsData.push({
+        name: faker.commerce.productName(),
+        sku: faker.random.number(),
+        price: fake.commerce.price(),
+        image: fake.image.fashion(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+   return queryInterface.bulkInsert('products', productsData);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('products', null, {});
+  }
+};
