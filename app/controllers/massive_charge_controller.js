91cf529e143094c537bfe1b70a2507451b4ac1e8
juakoke <jamunoz30@uc.cl>
2021-06-13 18:33:04 -0400

    succesfully products and failed products added

--- app/controllers/massive_charge_controller.js
+++ app/controllers/massive_charge_controller.js
@@ -9,18 +9,28 @@ async function create (req, res) {
     res.status(400).json({ state: 'F', error: 'No file uploaded' });
     return;
   }
-    var workbook = XLSX.read(req.files.excel.data, {type:'buffer'});
-    const workbookSheets = workbook.SheetNames;
-    const sheet = workbookSheets[0];
-    const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-    const firstRow = dataExcel[0];
-    if (!firstRow.name || !firstRow.sku || !firstRow.price || !firstRow.image) {
-      res.status(400).json({ state: 'F', error: 'Column are wrong' });
-      return;
-    }
-    dataExcel.forEach( async (row) => {
-      try {        
-        const last_product = await product.findOne({ where: { sku: row.sku } });
+
+  var workbook = XLSX.read(req.files.excel.data, {type:'buffer'});
+  const workbookSheets = workbook.SheetNames;
+  const sheet = workbookSheets[0];
+  const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+  const firstRow = dataExcel[0];
+  console.log(firstRow)
+  
+  var succes = 0;
+  var failed = 0;
+  var object_failed = [];
+
+  console.log("1")
+  console.log(dataExcel)
+  for (const row of dataExcel){
+    console.log("2")
+    try {        
+      const last_product = await product.findOne({ where: { sku: row.sku } });
+      if(!row.name || !row.sku || !row.price || !row.image) {
+        failed += 1;
+        object_failed.push(row);
+      } else{
         if (last_product) {
           //res.status(400).json({ state: 'F', error: 'That sku already exists' });
           await product.update({
@@ -29,24 +39,28 @@ async function create (req, res) {
             price: ((row.price) ? row.price : last_product.price),
             image: ((row.image) ? row.image : last_product.image),
           }, { where: { sku: last_product.sku } });
-          return;
+          succes += 1;
+        } else{
+          await product.create({
+            id: uuid(),
+            name: row.name,
+            sku: row.sku,
+            price: row.price,
+            image: row.image,
+          });
+          succes += 1;
         }
-    
-        await product.create({
-          id: uuid(),
-          name: row.name,
-          sku: row.sku,
-          price: row.price,
-          image: row.image,
-        });
-        return;
-      } catch (error) {
-        console.log('error');
       }
-    })  
-  
-  
-  res.send('ready');
+    } catch (error) {
+      failed += 1;
+      object_failed.push(row);
+      console.log('error');
+    }
+  }
+
+  res.status(200).json({"succesfully": succes, "failed": failed , "failed_products": object_failed 
+
+  })
 }
 
 module.exports = {
