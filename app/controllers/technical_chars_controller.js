46501c4f735a203dde7480ca89d3ca4843def994
cegarciav <cegarcia2@uc.cl>
2021-06-22 16:05:06 -0400

    setup: add documentation to chars endpoints

--- app/controllers/technical_chars_controller.js
+++ app/controllers/technical_chars_controller.js
@@ -1,7 +1,42 @@
 const { uuid } = require('uuidv4');
 const { technical_char } = require('../models');
 
-// CREATE
+/**
+ * @swagger
+ * /chars:
+ *  post:
+ *    tags:
+ *      - Technical Characteristics
+ *    summary: new technical characteristic
+ *    description: Allows to add a new technical characteristic to a given product
+ *    operationId: chars.create
+ *    produces:
+ *      - application/json
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *              - key
+ *              - value
+ *              - productId
+ *            properties:
+ *              key:
+ *                type: string
+ *              value:
+ *                type: string
+ *              productId:
+ *                type: string
+ *                format: uuidv4
+ *                description: id of an existing product
+ *    responses:
+ *      '201':
+ *        description: Technical characteristic created successfully
+ *      '400':
+ *        description: Some of the fields sent are not valid or missing
+ *      '500':
+ *        description: Internal server error
+ */
 async function create(req, res) {
   try {
     if (!req.body.key || !req.body.value || !req.body.productId) {
@@ -26,7 +61,23 @@ async function create(req, res) {
   }
 }
 
-// READ ALL
+/**
+ * @swagger
+ * /chars:
+ *  get:
+ *    tags:
+ *      - Technical Characteristics
+ *    summary: list of technical characteristics
+ *    description: Allows to retrieve a list of technical characteristics
+ *    operationId: chars.list
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      '200':
+ *        description: List of technical characteristics retrieved successfully
+ *      '500':
+ *        description: Internal server error
+ */
 async function show_all(req, res) {
   try {
     const technical_chars = await technical_char.findAll();
@@ -40,7 +91,37 @@ async function show_all(req, res) {
   }
 }
 
-// READ ONE
+/**
+ * @swagger
+ * /chars/show:
+ *  post:
+ *    tags:
+ *      - Technical Characteristics
+ *    summary: one technical characteristic
+ *    description: Allows to retrieve one technical characteristic
+ *    operationId: chars.show
+ *    produces:
+ *      - application/json
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *              - id
+ *            properties:
+ *              id:
+ *                type: string
+ *                format: uuidv4
+ *    responses:
+ *      '200':
+ *        description: Information of the technical characteristic retrieved successfully
+ *      '400':
+ *        description: Id not sent
+ *      '404':
+ *        description: Technical characteristic does not exist
+ *      '500':
+ *        description: Internal server error
+ */
 async function show(req, res) {
   try {
     if (!req.body.id) {
@@ -49,7 +130,7 @@ async function show(req, res) {
     }
     const technical_char_id = await technical_char.findOne({ where: { id: req.body.id } });
     if (!technical_char_id) {
-      res.status(400).json({ state: 'F', error: "Technical characteristic doesn't exist" });
+      res.status(404).json({ state: 'F', error: "Technical characteristic doesn't exist" });
       return;
     }
     res.status(200).json(technical_char_id);
@@ -62,7 +143,41 @@ async function show(req, res) {
   }
 }
 
-// UPDATE
+/**
+ * @swagger
+ * /chars:
+ *  patch:
+ *    tags:
+ *      - Technical Characteristics
+ *    summary: edit one technical characteristics
+ *    description: Allows to modify one technical characteristics
+ *    operationId: chars.modify
+ *    produces:
+ *      - application/json
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *              - id
+ *            properties:
+ *              id:
+ *                type: string
+ *                format: uuidv4
+ *              key:
+ *                type: string
+ *              value:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: Technical characteristic updated successfully
+ *      '400':
+ *        description: Id not sent
+ *      '404':
+ *        description: Technical characteristic does not exist
+ *      '500':
+ *        description: Internal server error
+ */
 async function update(req, res) {
   try {
     if (!req.body.id) {
@@ -71,12 +186,12 @@ async function update(req, res) {
     }
     const current_char = await technical_char.findOne({ where: { id: req.body.id } });
     if (!current_char) {
-      res.status(400).json({ state: 'F', error: 'Technical characteristic doesn\'t exist' });
+      res.status(404).json({ state: 'F', error: 'Technical characteristic doesn\'t exist' });
       return;
     }
     await technical_char.update({
-      key: ((req.body.key) ? req.body.key : current_char.key),
-      value: ((req.body.value) ? req.body.vale : current_char.value),
+      key: req.body.key || current_char.key,
+      value: req.body.value || current_char.value,
     }, { where: { id: req.body.id } });
 
     res.status(200).json({ state: 'OK' });
@@ -89,7 +204,37 @@ async function update(req, res) {
   }
 }
 
-// DELETE
+/**
+ * @swagger
+ * /chars:
+ *  delete:
+ *    tags:
+ *      - Technical Characteristics
+ *    summary: delete one technical characteristics
+ *    description: Allows to delete one technical characteristic
+ *    operationId: chars.destroy
+ *    produces:
+ *      - application/json
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *              - id
+ *            properties:
+ *              id:
+ *                type: string
+ *                format: uuidv4
+ *    responses:
+ *      '200':
+ *        description: Technical characteristic deleted successfully
+ *      '400':
+ *        description: Id not sent
+ *      '404':
+ *        description: Technical characteristic does not exist
+ *      '500':
+ *        description: Internal server error
+ */
 async function cdelete(req, res) {
   try {
     if (!req.body.id) {
@@ -98,7 +243,7 @@ async function cdelete(req, res) {
     }
     const current_char = await technical_char.findOne({ where: { id: req.body.id } });
     if (!current_char) {
-      res.status(400).json({ state: 'F', error: 'Technical characteristic doesn\'t exist' });
+      res.status(404).json({ state: 'F', error: 'Technical characteristic doesn\'t exist' });
       return;
     }
 
