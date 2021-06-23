f118df3f8fe4ff85d7202709bc3350421611bcd9
cegarciav <cegarcia2@uc.cl>
2021-06-23 01:12:46 -0400

    fix: solve forEach asynchronicity problem with Promise.all

--- app/controllers/massive_charge_controller.js
+++ app/controllers/massive_charge_controller.js
@@ -17,35 +17,37 @@ async function create(req, res) {
   let failed = 0;
   const object_failed = [];
 
-  dataExcel.forEach(async (row) => {
-    try {
-      const last_product = await product.findOne({ where: { sku: row.sku } });
-      if (!row.name || !row.sku || !row.price || !row.image) {
+  await Promise.all(
+    dataExcel.map(async (row) => {
+      try {
+        const last_product = await product.findOne({ where: { sku: row.sku } });
+        if (!row.name || !row.sku || !row.price || !row.image) {
+          failed += 1;
+          object_failed.push(row);
+        } else if (last_product) {
+          await product.update({
+            name: ((row.name) ? row.name : last_product.name),
+            sku: ((row.new_sku) ? row.new_sku : last_product.sku),
+            price: ((row.price) ? row.price : last_product.price),
+            image: ((row.image) ? row.image : last_product.image),
+          }, { where: { sku: last_product.sku } });
+          succes += 1;
+        } else {
+          await product.create({
+            id: uuid(),
+            name: row.name,
+            sku: row.sku,
+            price: row.price,
+            image: row.image,
+          });
+          succes += 1;
+        }
+      } catch (e) {
         failed += 1;
         object_failed.push(row);
-      } else if (last_product) {
-        await product.update({
-          name: ((row.name) ? row.name : last_product.name),
-          sku: ((row.new_sku) ? row.new_sku : last_product.sku),
-          price: ((row.price) ? row.price : last_product.price),
-          image: ((row.image) ? row.image : last_product.image),
-        }, { where: { sku: last_product.sku } });
-        succes += 1;
-      } else {
-        await product.create({
-          id: uuid(),
-          name: row.name,
-          sku: row.sku,
-          price: row.price,
-          image: row.image,
-        });
-        succes += 1;
       }
-    } catch (e) {
-      failed += 1;
-      object_failed.push(row);
-    }
-  });
+    }),
+  );
 
   res.status(200).json({
     succesfully: succes,
