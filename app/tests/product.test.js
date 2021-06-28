391db31ea2159f64e125132ecc2e8da56733bd28
cegarciav <cegarcia2@uc.cl>
2021-06-27 21:44:41 -0400

    feat: add test with payment methods in products

--- app/tests/product.test.js
+++ app/tests/product.test.js
@@ -3,8 +3,16 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('Product CRUD Testing', () => {
+  let paymentMethod;
+  beforeAll(async () => {
+    const res = await request(app)
+      .post('/payment-methods')
+      .send({
+        name: 'Credit card',
+      });
+    paymentMethod = res.body.paymentMethod;
+  });
   // CREATE
-
   it('should fail creating fields not provided', async () => {
     const res = await request(app)
       .post('/products')
@@ -67,6 +75,21 @@ describe('Product CRUD Testing', () => {
     expect(res.body.error).toEqual('Invalid fields');
   });
 
+  it('should fail creating a product because the payment methods created are not valid', async () => {
+    const res = await request(app)
+      .post('/products')
+      .send({
+        name: 'test_product',
+        sku: 12345678,
+        price: 10000,
+        image: 'https://www.lapolar.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dw1c04210e/images/large/23019272.jpg?sw=1200&sh=1200&sm=fit',
+        paymentMethodIds: ['66db6603-ec5c-4eef-85c2-748a315931a1'],
+      });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.state).toEqual('F');
+    expect(res.body.error).toEqual('Some of the payment methods sent are not valid');
+  });
+
   it('should create a new product', async () => {
     const res = await request(app)
       .post('/products')
@@ -75,6 +98,7 @@ describe('Product CRUD Testing', () => {
         sku: 12345678,
         price: 10000,
         image: 'https://www.lapolar.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dw1c04210e/images/large/23019272.jpg?sw=1200&sh=1200&sm=fit',
+        paymentMethodIds: [paymentMethod.id],
       });
     expect(res.statusCode).toEqual(201);
     expect(res.body.state).toEqual('OK');
@@ -183,6 +207,29 @@ describe('Product CRUD Testing', () => {
     expect(res.body.state).toEqual('OK');
   });
 
+  it('should fail updating payment methods because some ids are not valid', async () => {
+    const res = await request(app)
+      .patch('/products')
+      .send({
+        sku: 12345678,
+        paymentMethodIds: ['66db6603-ec5c-4eef-85c2-748a315931a1', paymentMethod.id],
+      });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.state).toEqual('F');
+    expect(res.body.error).toEqual('Some of the payment methods sent are not valid');
+  });
+
+  it('should update the payment methods of one product', async () => {
+    const res = await request(app)
+      .patch('/products')
+      .send({
+        paymentMethodIds: [paymentMethod.id],
+        sku: 12345678,
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.state).toEqual('OK');
+  });
+
   // DELETE
   it('should fail deleting one product because sku is not sent', async () => {
     const res = await request(app)
@@ -214,5 +261,11 @@ describe('Product CRUD Testing', () => {
     expect(res.body.state).toEqual('OK');
   });
 
-  afterAll(async () => { });
+  afterAll(async () => {
+    await request(app)
+      .delete('/payment-methods')
+      .send({
+        name: 'Credit card',
+      });
+  });
 });
