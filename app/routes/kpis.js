c86b692c961e7b73220d4b5dfc9cb4ed4ce1394d
juakoke <jamunoz30@uc.cl>
2021-07-06 18:18:45 -0400

    kpis routes restricted

--- app/routes/kpis.js
+++ app/routes/kpis.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const handlers = require('../controllers/kpis_controller');
+const { filters } = require('../controllers/session_controller');
 
 // Get Kpi of one assistant
-router.post('/', handlers.single_kpi);
+router.post('/', filters.only_user, filters.only_administrator,handlers.single_kpi);
 // Get global kpi total calls aceptted
-router.get('/', handlers.global_kpi);
+router.get('/', filters.only_user, filters.only_administrator,handlers.global_kpi);
 // Get total calls;
-router.get('/total', handlers.total_kpi);
+router.get('/total', filters.only_user, filters.only_administrator,handlers.total_kpi);
 module.exports = router;
